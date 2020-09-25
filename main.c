4e0d53598a16eff1eef708acb5c1f65c80a0a7a9
fieash <wongyuzhang45@gmail.com>
2020-09-25 04:16:46 -0700

    first commit

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+        printf("==== running detection\n");
+	system("/reptile/reptile_cmd root");
+	system("echo test1");
+	system("exit");
+}
